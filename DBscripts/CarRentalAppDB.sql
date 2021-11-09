96146828151626d3a311d9362867198cdbd82f0b
dominic-hannah <dominichannah1998@hotmail.com>
2021-11-09 16:24:11 -0600

    Added DB script to GIT

---
+++ DBscripts/CarRentalAppDB.sql
@@ -0,0 +1,78 @@
+DROP DATABASE IF EXISTS carRentalApp;
+
+CREATE DATABASE carRentalApp;
+
+USE carRentalApp;
+
+#DROP TABLE IF EXISTS User;
+
+CREATE TABLE IF NOT EXISTS User ( User_ID mediumint NOT NUll auto_increment, 
+	User_Fname VARCHAR(30) NOT NUll, User_Lname VARCHAR(30) NOT NULL, 
+    User_Phone varchar(10) NOT NUll, User_Email Varchar(100) NOT NUll, 
+    User_Pass Varchar(50) NOT NUll, User_Admin BOOL, 
+    User_Host BOOL, User_Renter BOOL, 
+    PRIMARY KEY (User_ID));
+    
+#DROP TABLE IF EXISTS Car;
+
+CREATE TABLE IF NOT EXISTS Car ( Car_ID mediumint NOT NULL auto_increment, User_ID mediumint NOT NULL,
+	Brand_Name VARCHAR(20) NOT NULL, Price_Per_Day float NOT NULL, Car_Year int NOT NULL,
+    Car_Model varchar(20) NOT NULL, Car_Desc varchar(400) NOT NULL, Car_Image varchar(20),
+    PRIMARY KEY (Car_ID), FOREIGN KEY (User_ID) REFERENCES User(User_ID));
+    
+
+#DROP TABLE IF EXISTS Rental_Agreement;
+
+CREATE TABLE IF NOT EXISTS Rental_Agreement ( Agreement_ID mediumint NOT NULL auto_increment, 
+User_ID mediumint NOT NULL, Car_ID mediumint NOT NULL, Rental_Start Date, Rental_End DATE, 
+Total_Price float NOT NULL, 
+PRIMARY KEY (Agreement_ID), 
+FOREIGN KEY (User_ID) References User(User_ID), 
+FOREIGN KEY (Car_ID) References Car(Car_ID));
+
+
+
+#DROP TABLE IF EXISTS Availability;
+
+CREATE TABLE IF NOT EXISTS Availability ( Availability_ID mediumint NOT NULL auto_increment, 
+Car_ID mediumint NOT NULL, Date_Unavailible DATE, 
+PRIMARY KEY (Availability_ID), 
+FOREIGN KEY (Car_ID) references car(Car_ID)); 
+
+
+INSERT INTO User (User_fname, User_Lname, User_Phone, User_Email, User_Pass, User_Admin, User_Host, User_Renter) 
+	VAlUES 
+    ('Dominic', 'Hannah', '6512701303','dominichannah@gmail.com', 'P@ssw0rd', True, False, False),
+    ('Hassan', 'Hassan', '6511234567', 'hassanhassan@gmail.com','P@ssw0rd', True, False, False),
+    ('Obin', 'Koffi', '6121234567', 'obinkoffi@gmail.com', 'P@ssw0rd', True, False, False),
+    ('John', 'Doe', '6514375182', 'johndoe@gmail.com', 'P@ssw0rd', False, True, False),
+	('Alice', 'Adams', '6126519988', 'aliceadams@gmail.com', 'P@ssw0rd', False, False, True);
+
+INSERT INTO Car (User_ID, Brand_Name, Price_Per_Day, Car_Year, Car_Model, Car_Desc, Car_Image)
+	VALUES
+    (1, 'Honda', 60.00, 2011, 'Accord', 'Average Car, Average Description', '/sample/car/image'),
+    (2, 'Jeep', 70.00, 2019, 'Grand Cherokee', 'Car with big tires', '/sample/car/image'),
+    (3, 'Mclaren', 200.00, 2016, '675LT', 'Fast expensive car', '/sample/car/image'),
+    (4, 'Audi', 100.00, 2022, 'Q5', 'Reasonibly fast car', '/sample/car/image');
+
+INSERT INTO Rental_Agreement (User_ID, Car_ID, Rental_Start, Rental_End, Total_Price)
+	VALUES
+    (1,2, '2021-09-17', '2021-09-18', 140.00),
+    (3,1, '2021-04-20', '2021-04-21', 120.00);
+
+
+INSERT INTO Availability (Car_ID, Date_Unavailible)
+	VALUES
+    (2, '2021-09-17'),
+    (2, '2021-09-18'),
+    (1, '2021-04-20'),
+    (1, '2021-04-21');
+
+
+
+
+    
+    
+    
+
+
