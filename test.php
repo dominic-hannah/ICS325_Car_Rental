1362b8f2963b66921b905cc6fd45caac05355851
dominic-hannah <dominichannah1998@hotmail.com>
2021-11-11 15:23:07 -0600

    messing around
    Added test page to index.html to mess with pulling data from the database

---
+++ test.php
@@ -0,0 +1,27 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "","carrentalapp");
+if ($mysqli->connect_errno){
+    echo "Failed";
+}
+
+$sql = "SELECT * FROM car";
+$result = mysqli_query($mysqli,$sql);
+
+
+
+while($row = $result->fetch_row()){
+    echo $row[0];
+    
+    echo $row[1];
+    
+    echo $row[2];
+        
+    echo $row[3];
+        
+    echo $row[4];
+        
+    echo $row[5];
+    echo $row[6];
+    echo $row[7];
+    echo "<br>";
+}
