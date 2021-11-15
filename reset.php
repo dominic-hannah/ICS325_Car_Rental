0a5cef5f6b05152baae8400a19e9f215a1d48f3d
obindev <36115112+obindev@users.noreply.github.com>
2021-11-15 16:08:27 -0600

    2 new page basic interface only. work in progress push update in 32min

---
+++ reset.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>MN Car Solution|Sign In</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+    button {
+  padding: 10px 19px 10px 19px;
+  color: #FFF;
+  background-color: #4bc970;
+  font-size: 18px;
+  text-align: center;
+  font-style: normal;
+  border-radius: 5px;
+  width: 50%;
+  border: 1px solid #3ac162;
+  border-width: 1px 1px 3px;
+  box-shadow: 0 -1px 0 rgba(255,255,255,0.1) inset;
+  margin-bottom: 10px;
+}
+
+fieldset {
+  margin-bottom: 30px;
+  border: none;
+}
+
+legend {
+  font-size: 1.4em;
+  margin-bottom: 10px;
+}
+
+label {
+  display: block;
+  margin-bottom: 8px;
+}
+
+label.light {
+  font-weight: 300;
+  display: inline;
+}
+    .number {
+  background-color: #5fcf80;
+  color: #fff;
+  height: 30px;
+  width: 30px;
+  display: inline-block;
+  font-size: 0.8em;
+  margin-right: 4px;
+  line-height: 30px;
+  text-align: center;
+  text-shadow: 0 1px 0 rgba(255,255,255,0.2);
+  border-radius: 100%;
+}
+
+@media screen and (min-width: 480px) {
+
+  form {
+    max-width: 480px;
+  }
+
+  </style>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h1>MN CAR SOLUTION</h1>
+  <p>We are the leading brand in automotive rental service!</p> 
+</div>
+ <div class="container">
+  <h2>Reset password</h2>
+  <ul class="nav nav-tabs" role="tablist">
+    <li><a href="../assignment3/index.html">Home</a></li>
+     <li class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+      Products <span class="caret"></span></a>
+      <ul class="dropdown-menu" role="menu">
+        <li><a href="../assignment3/RentACar.html">Rent A car</a></li>
+        <li><a href="../assignment3/RentYourCar.html">Rent your car</a></li>                       
+      </ul>
+    </li>
+     <li class="dropdown">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+      Service <span class="caret"></span></a>
+      <ul class="dropdown-menu" role="menu">
+        <li class="active"><a href="#">Sign in</a></li>
+        <li><a href="../assignment3/registration.php">Create account</a></li>                      
+      </ul>
+    </li>
+    <li><a href="../assignment3/AboutUs.html">About Us & FAQs</a></li>
+    <li><a href="../assignment3/ContactUs.html">Contact Us</a></li>   
+  </ul>
+</div>
+  </div>
+  <div class="container">
+    <form action="../assignment3/confirmReset.php" method="POST" class="was-validated">
+        <fieldset>
+          <legend><span class="number">User</span>Reset Password</legend>
+        <label for="mail">Email:</label>
+          <input type="email" id="mail" name="user_email">
+          <label for="password">New Password:</label>
+          <input type="password" id="password" name="user_password">
+		  <label for="password">confirm Password:</label>
+          <input type="password" id="password" name="user_password">
+        </fieldset>
+          <button type="submit">Update Password</button><br>
+		
+          
+    </form>
+</div>
+
+</body>
+</html>
