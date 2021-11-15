afb8976a8ba8f1f8b7bbeb4542aa5d4228b49eda
dominic-hannah <dominichannah1998@hotmail.com>
2021-11-15 12:52:49 -0600

    moving things around

--- registration.php
+++ registration.php
@@ -168,7 +168,7 @@ icon-container {
 </div>
   
 <div class="container">
-  <form action="testaccount.php" method="post">
+  <form action="registration.php" method="post">
       
         <h1>USER PROFILE SET-UP</h1>
         
@@ -284,6 +284,48 @@ icon-container {
       </form>
 
 </div>
+<?php
+        
+        if(isset($_POST['user_email'])){
+            $email = $_POST['user_email'];
+            echo $email;
+        }
+        if(isset($_POST['user_fname'])){
+            $fname = $_POST['user_fname'];
+            echo $fname;
+        }
+        if(isset($_POST['user_lname'])){
+            $lname = $_POST['user_lname'];
+            echo $lname;
+        }
+
+        if( $_POST['user_password1'] == $_POST['user_password2']){
+                $password = $_POST['user_password1'];
+                echo $password;
+        }else {
+            echo "oops";
+        }
+
+        echo "".$_POST['user_password1']." <br>";
+        echo $_POST['user_password2'];
+
+        if (isset($_POST['user_phone'])){
+            $user_phone = $_POST['user_phone'];
+            echo $user_phone;
+        }
+        
+
+        $mysqli = new mysqli("localhost", "root", "","carrentalapp");
+        if ($mysqli->connect_errno){
+          echo "Failed";
+        }
+
+
+        $sql = "INSERT INTO user (User_Fname, User_Lname, User_Phone, User_Email, User_Pass) VALUES ('".$fname."', '".$lname."', '".$user_phone."', '".$email."', '".$password."')";
+        
+        $result = mysqli_query($mysqli,$sql);
+
+?>
 
 </body>
 </html>
