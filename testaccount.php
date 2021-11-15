afbce22589070d88d459a5f73f2e3b24bc2d1e7f
dominic-hannah <dominichannah1998@hotmail.com>
2021-11-14 22:33:34 -0600

    idk

---
+++ testaccount.php
@@ -0,0 +1,26 @@
+<?php
+        
+
+
+        if(isset($_POST['user_email'])){
+            $email = $_POST['user_email'];
+            echo $email;
+        }
+
+        if(isset($_POST['password'])){
+            $password = $_POST['password'];
+        }
+
+        if (isset($_POST['name'])){
+            $name = $_POST['name'];
+        }
+
+        if (isset($_POST['date'])){
+            $date = $_date['date'];
+        }
+
+        if (isset($_POST['address'])){
+            $address = $_address['address'];
+        }
+
+?>
